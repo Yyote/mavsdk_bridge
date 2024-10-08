36a506c7ee66b647fb5b2e917289618767466275
Leshchev <yyootttaa@gmail.com>
2024-10-08 13:49:12 +0300

    Minor additions

--- src/precision_landing_transmitter.cpp
+++ src/precision_landing_transmitter.cpp
@@ -24,7 +24,7 @@
 #include "privyaznik_msgs/action/command.hpp"
 #include "tf2/LinearMath/Matrix3x3.h"
 #include "tf2/LinearMath/Quaternion.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 #include "std_msgs/msg/string.hpp"
 
 #include <mavsdk/plugins/mission_raw/mission_raw.h>
